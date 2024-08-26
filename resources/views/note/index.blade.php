ab26836e7de473e7d0f9c80e5d3d290a4abeb69e
minooaung <aungminoo83@gmail.com>
2024-08-27 10:11:39 +1200

    modified:   package-lock.json 	modified:   package.json 	modified:   resources/css/app.css 	modified:   resources/views/auth/login.blade.php 	modified:   resources/views/note/index.blade.php

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -3,27 +3,42 @@
         <a href="{{ route('note.create') }}" class="new-note-btn">
             Create New Note
         </a>
+        
         <div class="notes">
-            @foreach ($notes as $note)
-                <div class="note">
-                    <div class="note-body">                        
-                        {{ Str::words($note->note, 30) }}
-                    </div>
-                    <div class="note-buttons">
-                        <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
-                        <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
-                        <form action="{{ route('note.destroy', $note) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="note-delete-button">Delete</button>
-                        </form>
-                    </div>
-                </div>
-            @endforeach
+            <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Note</th>
+                    <th scope="col">Created At</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($notes as $note)
+                    <tr>
+                      <th scope="row">{{$note->id}}</th>
+                      <td>{{Str::words($note->note, 30)}}</td>
+                      <td>{{$note->created_at}}</td>
+                      <td>
+                        <div class="note-buttons">
+                            <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
+                            <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                            <form action="{{ route('note.destroy', $note) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="note-delete-button">Delete</button>
+                            </form>
+                        </div>
+                      </td>
+                    </tr>
+                    @endforeach
+            </table>
+            <div>
+                {{ $notes->links('pagination::bootstrap-5') }}
+            </div>
         </div>
-
-        <div class="p-6">
-            {{ $notes->links() }}
-        </div>
+        
+        
     </div>
 </x-app-layout>
